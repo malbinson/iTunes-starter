4448a9e21c6f4275ae86da6511ce26cc65dd2566
Matt Albinson <mattalbinson@berkeley.net>
2026-01-29 13:13:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function run() {
 
-    $.ajax({});
+    //need to add term and limit to the query parameters 
+    let url = "https://itunes.apple.com/search?entity=musicTrack&attribute=allArtistTerm?entity=musicTrack&attribute=allArtistTerm"
+
+    //connects to iTunes api and passes off to process function on success
+    $.ajax({
+        url: url,
+        dataType: "json",
+        success: process
+    });
+    
 }
 
 function process(data) {
@@ -8,6 +17,8 @@ function process(data) {
 
     let o = "";
 
+    //builds a giant html string - you need to add the fields 
+    //from iTunes into the table and then....
     for(let p=0;p<data.results.length;p++) {
         o += "<tr>";
         o += "<td>" + + "</td>";
@@ -17,6 +28,7 @@ function process(data) {
         o += "</tr>";
     }
 
+    //adds the created table to the page
     let table = document.getElementById("output");
     table.innerHTML = o;
     table.style.display = "table";
